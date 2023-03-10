d611254c4440fe2a8d09a36908668fd139a0bc2a
QmBo <qrioflat@gmail.com>
2023-03-10 14:00:45 +0600

    Subscribe adn unsubscribe command added.

--- src/main/java/ru/qmbo/telegram_messages_sender/service/TelegramBotMessages.java
+++ src/main/java/ru/qmbo/telegram_messages_sender/service/TelegramBotMessages.java
@@ -2,16 +2,17 @@ package ru.qmbo.telegram_messages_sender.service;
 
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.UpdatesListener;
+import com.pengrad.telegrambot.model.Message;
 import com.pengrad.telegrambot.model.Update;
 import com.pengrad.telegrambot.request.SendMessage;
-
 import lombok.extern.log4j.Log4j2;
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import static java.lang.String.*;
 
 /**
  * TelegramBotMessages
@@ -26,7 +27,13 @@ public class TelegramBotMessages {
     /**
      * The constant TEMPLATE_URL.
      */
-    public static final String TEMPLATE_URL = "%s/calc?chatId=%s&amount=%s";
+    public static final String CALC_TEMPLATE_URL = "%s/calc?chatId=%s&amount=%s";
+    public static final String SUBSCRIBE_TEMPLATE_URL = "%s/users/add?chatId=%s";
+    public static final String UNSUBSCRIBE_TEMPLATE_URL = "%s/users/dell?chatId=%s";
+    public static final String SUBSCRIBE = "/subscribe";
+    public static final String UNSUBSCRIBE = "/unsubscribe";
+    public static final String SORRY = "Пока я понимаю только целые числа =(";
+
     /**
      * The Host.
      */
@@ -71,25 +78,38 @@ public class TelegramBotMessages {
      * @param update the update
      */
     public void requestParser(Update update) {
-        if (update.message() != null) {
-            boolean parsOk = false;
-            try {
-                int parseInt = Integer.parseInt(update.message().text());
-                this.sendRequest(parseInt, update.message().chat().id());
-                parsOk = true;
-            } catch (Exception e) {
-                log.warn("Number format exception: {}", e.getMessage());
-            }
-            if (!parsOk) {
-                this.messageSend(update.message().chat().id(), "Пока я понимаю только целые числа =(");
+        Message message = update.message();
+        if (message != null) {
+            if (message.text().startsWith("/")) {
+                this.doCommand(update);
+            } else {
+                boolean parsOk = false;
+                try {
+                    int parseInt = Integer.parseInt(message.text());
+                    this.sendRequest(format(CALC_TEMPLATE_URL, host, message.chat().id(), parseInt));
+                    parsOk = true;
+                } catch (Exception e) {
+                    log.warn("Number format exception: {}", e.getMessage());
+                }
+                if (!parsOk) {
+                    this.messageSend(message.chat().id(), SORRY);
+                }
             }
         }
     }
 
+    private void doCommand(Update update) {
+        Message message = update.message();
+        if (SUBSCRIBE.equals(message.text())) {
+            this.sendRequest(format(SUBSCRIBE_TEMPLATE_URL, host, message.chat().id()));
+        } else if (UNSUBSCRIBE.equals(message.text())) {
+            this.sendRequest(format(UNSUBSCRIBE_TEMPLATE_URL, host, message.chat().id()));
+        }
+    }
 
-    private void sendRequest(int amount, Long chatId) {
-        RestTemplate rt =  new RestTemplate();
-        final String uri = String.format(TEMPLATE_URL, host, chatId, amount);
+
+    private void sendRequest(String uri) {
+        RestTemplate rt = new RestTemplate();
         try {
             rt.getForEntity(uri, String.class);
         } catch (Exception e) {
@@ -97,5 +117,4 @@ public class TelegramBotMessages {
             log.error("Uri is: {}", uri);
         }
     }
-    
 }
